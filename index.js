aa1b57a0de3a9ba7dc6180df586fceeaa9366d5d
Spy <Spyngunga@gmail.com>
2023-10-29 05:06:08 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+const writeCards = ( a,e ) => {
+  let msg = [];
+for(let i=0; i<a.length; i++){
+  msg.push( `Thank you, ${a[i]}, for the wonderful ${e} gift!`);
+}
+return msg;
+}
+const countDown = (count) =>
+{
+  let i= count;
+  while(i >= 0){
+      console.log(count);
+      i--;
+      count = count -1;
+  }
+}
+console.log(countDown(10))
